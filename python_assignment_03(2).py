e956662a8f076d79eb36f9b52f745b2fdb44722b
vishwajitpratapsingh17-ship-it <vishwajitpratapsingh17@gmail.com>
2025-07-26 10:12:29 +0530

    Add files via upload

---
+++ python_assignment_03(2).py
@@ -0,0 +1,16 @@
+import math
+n=int(input("Please enter a random number? :  "))
+def sqrt(n):
+    return math.sqrt(n)
+result = sqrt(n)
+print(result)
+print()
+def log(n):
+    return math.log(n)
+result = log(n)
+print(result)
+print()
+def sin(n):
+    return math.sin(n)
+result = sin(n)
+print(result)
